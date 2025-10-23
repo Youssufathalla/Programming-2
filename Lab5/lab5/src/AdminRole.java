8c2261ee4c4b312518bb69886e765c862a95a7ab
Omar-Sharkawy6 <omarsharkawy66@gmail.com>
2025-10-23 20:55:12 +0300

    old code updated

---
+++ Lab5/lab5/src/AdminRole.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+
+
+public class AdminRole {
+
+    private database db;
+
+    public AdminRole() {
+        this.db = new EmployeeUserDatabase("Employees.txt");
+        this.db.readFromFile();
+    }
+
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        if (!db.contains(employeeId)) {
+            EmployeeUser e = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+            db.insertRecord(e);
+        }
+    }
+
+      public EmployeeUser[] getListOfEmployees() {
+          ArrayList<item> x=db.returnAllRecords();
+          EmployeeUser[]y =new EmployeeUser[x.size()];
+        return (EmployeeUser[]) db.returnAllRecords().toArray(y);
+   
+    }
+
+    public void removeEmployee(String key) {
+        db.deleteRecord(key);
+    }
+
+    public void logout() {
+        db.saveToFile();
+    }
+}

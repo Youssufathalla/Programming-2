d844ce17c4d80615c72f187415538dbeba3ce934
Youssufathalla <Youssufathalla@gmail.com>
2025-11-14 21:22:49 +0200

    new classes

---
+++ lab 7/lab 7/src/lab/pkg7/course.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg7;
+
+import java.util.*;
+
+
+
+
+/**
+ *
+ * @author youssufathalla
+ */
+public class course {
+    private String courseId;
+    private String title;
+    private String description;
+    private String instructorId;
+    private List<Lesson> lessons;
+    private List<String> enrolledStudents;
+    
+}

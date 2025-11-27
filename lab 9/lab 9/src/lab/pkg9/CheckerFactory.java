87299ba50d74fa204eb713ef772259f8118f3d88
Youssufathalla <Youssufathalla@gmail.com>
2025-11-27 18:25:51 +0200

    lolll

---
+++ lab 9/lab 9/src/lab/pkg9/CheckerFactory.java
@@ -0,0 +1,15 @@
+package lab.pkg9;
+
+
+public class CheckerFactory {
+
+    // type: 0 = row, 1 = col, 2 = box
+    public static Checker createChecker(int type, int[][] board, int index) {
+        switch (type) {
+            case 0: return new RowChecker(board, index);
+            case 1: return new ColChecker(board, index);
+            case 2: return new BoxChecker(board, index);
+            default: throw new IllegalArgumentException("Invalid checker type");
+        }
+    }
+}

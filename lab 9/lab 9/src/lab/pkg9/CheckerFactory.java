63abf32b0c3136a9610a7a8a5da5910640b80385
seifrashed005 <seifrashed05@gmail.com>
2025-11-27 19:15:02 +0200

    checker factory

--- lab 9/lab 9/src/lab/pkg9/CheckerFactory.java
+++ lab 9/lab 9/src/lab/pkg9/CheckerFactory.java
@@ -1,15 +1,21 @@
 package lab.pkg9;
 
-
 public class CheckerFactory {
 
-    // type: 0 = row, 1 = col, 2 = box
-    public static Checker createChecker(int type, int[][] board, int index) {
+    public static AbstractChecker createChecker(int type, int[][] board, DuplicateResult result) {
+
         switch (type) {
-            case 0: return new RowChecker(board, index);
-            case 1: return new ColumnChecker(board, index);
-            case 2: return new BoxChecker(board, index);
-            default: throw new IllegalArgumentException("Invalid checker type");
+            case 0:
+                return new RowChecker(board, result);
+
+            case 1:
+                return new ColumnChecker(board, result);
+
+            case 2:
+                return new BoxChecker(board, result);
+
+            default:
+                throw new IllegalArgumentException("Invalid checker type");
         }
     }
 }

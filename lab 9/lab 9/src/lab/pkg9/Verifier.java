8145b1a1aae1cda309edae60574c10bd638f4129
Youssufathalla <Youssufathalla@gmail.com>
2025-11-27 19:05:49 +0200

    Update Verifier.java

--- lab 9/lab 9/src/lab/pkg9/Verifier.java
+++ lab 9/lab 9/src/lab/pkg9/Verifier.java
@@ -1,6 +1,5 @@
 package lab.pkg9;
 
-
 import java.util.ArrayList;
 
 public class Verifier {
@@ -20,32 +19,38 @@ public class Verifier {
             for (int i = 0; i < 9; i++) {
                 RowChecker w = new RowChecker(board, i);
                 w.run();
-                if (!w.isValid()) finalValid = false;
+                if (!w.isValid()) {
+                    finalValid = false;
+                }
                 finalErrors.addAll(w.getErrors());
             }
 
             for (int j = 0; j < 9; j++) {
-                ColChecker w = new ColChecker(board, j);
+                ColumnChecker w = new ColumnChecker(board, j);
                 w.run();
-                if (!w.isValid()) finalValid = false;
+                if (!w.isValid()) {
+                    finalValid = false;
+                }
                 finalErrors.addAll(w.getErrors());
             }
 
             for (int b = 0; b < 9; b++) {
                 BoxChecker w = new BoxChecker(board, b);
                 w.run();
-                if (!w.isValid()) finalValid = false;
+                if (!w.isValid()) {
+                    finalValid = false;
+                }
                 finalErrors.addAll(w.getErrors());
             }
-        }
-
-        else if (mode == 3) {
+        } else if (mode == 3) {
 
             Thread tRows = new Thread(() -> {
                 for (int i = 0; i < 9; i++) {
                     RowChecker w = new RowChecker(board, i);
                     w.run();
-                    if (!w.isValid()) finalValid = false;
+                    if (!w.isValid()) {
+                        finalValid = false;
+                    }
                     synchronized (finalErrors) {
                         finalErrors.addAll(w.getErrors());
                     }
@@ -54,9 +59,11 @@ public class Verifier {
 
             Thread tCols = new Thread(() -> {
                 for (int j = 0; j < 9; j++) {
-                    ColChecker w = new ColChecker(board, j);
+                    ColumnChecker w = new ColumnChecker(board, j);
                     w.run();
-                    if (!w.isValid()) finalValid = false;
+                    if (!w.isValid()) {
+                        finalValid = false;
+                    }
                     synchronized (finalErrors) {
                         finalErrors.addAll(w.getErrors());
                     }
@@ -67,51 +74,67 @@ public class Verifier {
                 for (int b = 0; b < 9; b++) {
                     BoxChecker w = new BoxChecker(board, b);
                     w.run();
-                    if (!w.isValid()) finalValid = false;
+                    if (!w.isValid()) {
+                        finalValid = false;
+                    }
                     synchronized (finalErrors) {
                         finalErrors.addAll(w.getErrors());
                     }
                 }
             });
 
-            tRows.start(); 
-            tCols.start(); 
+            tRows.start();
+            tCols.start();
             tBoxes.start();
 
-            tRows.join();  
-            tCols.join();  
+            tRows.join();
+            tCols.join();
             tBoxes.join();
-        }
-
-        else if (mode == 27) {
+        } else if (mode == 27) {
 
             Checker[] checkers = new Checker[27];
             Thread[] threads = new Thread[27];
             int idx = 0;
 
-            for (int i = 0; i < 9; i++) checkers[idx++] = new RowChecker(board, i);
-            for (int j = 0; j < 9; j++) checkers[idx++] = new ColChecker(board, j);
-            for (int b = 0; b < 9; b++) checkers[idx++] = new BoxChecker(board, b);
+            for (int i = 0; i < 9; i++) {
+                checkers[idx++] = new RowChecker(board, i);
+            }
+            for (int j = 0; j < 9; j++) {
+                checkers[idx++] = new ColumnChecker(board, j);
+            }
+            for (int b = 0; b < 9; b++) {
+                checkers[idx++] = new BoxChecker(board, b);
+            }
 
-            for (int i = 0; i < 27; i++) threads[i] = new Thread(checkers[i]);
+            for (int i = 0; i < 27; i++) {
+                threads[i] = new Thread(checkers[i]);
+            }
 
-            for (Thread t : threads) t.start();
-            for (Thread t : threads) t.join();
+            for (Thread t : threads) {
+                t.start();
+            }
+            for (Thread t : threads) {
+                t.join();
+            }
 
             for (Checker c : checkers) {
-                if (!c.isValid()) finalValid = false;
+                if (!c.isValid()) {
+                    finalValid = false;
+                }
                 finalErrors.addAll(c.getErrors());
             }
-        }
-
-        else {
+        } else {
             return "Invalid mode. Use 0, 3, or 27.\n";
         }
 
-        if (finalValid) return "VALID\n";
+        if (finalValid) {
+            return "VALID\n";
+        }
 
         StringBuilder sb = new StringBuilder("INVALID\n");
-        for (String e : finalErrors) sb.append(e).append("\n");
+        for (String e : finalErrors) {
+            sb.append(e).append("\n");
+        }
         return sb.toString();
     }
 }

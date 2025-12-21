c44a1ea5ac334085c54c203517a103c823e92ac0
Youssufathalla <Youssufathalla@gmail.com>
2025-12-21 15:48:13 +0200

    class game

--- Lab10/src/lab10/Game.java
+++ Lab10/src/lab10/Game.java
@@ -1,12 +1,42 @@
+package lab.pkg9;
+
 public class Game {
 
-    private final String boardContent;
+    private int[][] board;
+    private String difficulty;
 
-    public Game(String boardContent) {
-        this.boardContent = boardContent;
+    public Game(int[][] board, String difficulty) {
+        this.board = board;
+        this.difficulty = difficulty;
     }
 
-    public String getBoardContent() {
-        return boardContent;
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Difficulty: ").append(difficulty).append("\n");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append(board[i][j]);
+                if (j < 8) sb.append(", ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }

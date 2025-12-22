239a2afdbec0725f106f4663c7e237f055f374b2
Youssufathalla <Youssufathalla@gmail.com>
2025-12-22 07:13:37 +0200

    Update Main.java

--- Lab10/src/View/Main.java
+++ Lab10/src/View/Main.java
@@ -17,12 +17,6 @@ Controllable controller = ControllerFacade.getInstance();
     Catalog catalog = view.getCatalog();
     new StarterFrame(view).setVisible(true);
 
-    System.out.println("Game in progress: " + catalog.hasUnfinishedGame());
-    System.out.println("All modes exist: " + catalog.hasGamesInDifficultyFiles());
-
-        int[][] board = BoardReader.read(args[0]);
-        Verifier verifier = new Verifier(board);
-
-        System.out.println(verifier.run());
+   
     }
 }

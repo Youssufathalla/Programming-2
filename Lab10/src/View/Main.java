fe8bf2292dbf7add0527a3c3991410fb8a233ac2
Youssufathalla <Youssufathalla@gmail.com>
2025-12-22 07:07:50 +0200

    Update Main.java

--- Lab10/src/View/Main.java
+++ Lab10/src/View/Main.java
@@ -1,11 +1,7 @@
 package View;
-import Controller.GameController;
 import Controller.Controllable;
 import Controller.ControllerFacade;
 import Model.BoardReader;
-import View.GameView;
-import View.Viewable;
-import Model.Catalog;
 import Model.Catalog;
 import Model.Verifier;
 
@@ -19,6 +15,7 @@ Controllable controller = ControllerFacade.getInstance();
     Viewable view = new GameView(controller);
 
     Catalog catalog = view.getCatalog();
+    new StarterFrame(view).setVisible(true);
 
     System.out.println("Game in progress: " + catalog.hasUnfinishedGame());
     System.out.println("All modes exist: " + catalog.hasGamesInDifficultyFiles());
